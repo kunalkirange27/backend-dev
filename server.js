8bc180326006c58f6d6d1135d96f9368ec4da572
kunalkirange27 <kunalkirange777@gmail.com>
2025-03-25 23:18:07 +0530

    minor changes

--- server.js
+++ server.js
@@ -404,6 +404,10 @@ app.post("/store-merged-data", async (req, res) => {
 });
 
 
+app.get("/test", (req, res) => {
+    res.send("Server is running!");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
